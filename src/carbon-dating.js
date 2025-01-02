d666a154434e3328b286fc71d0e3cc99b22d499f
Sartre666 <delphi100500@gmail.com>
2025-01-02 21:56:08 +0300

    feat: add carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || sampleActivity.trim()?.length === 0 || isNaN(sampleActivity) || +sampleActivity <= 0 || +sampleActivity > MODERN_ACTIVITY) {
+    return false;
+  }
+  const K_CONST = 0.693 / HALF_LIFE_PERIOD;
+
+  const age = Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / K_CONST);
+  return age;
 }
 
 module.exports = {
