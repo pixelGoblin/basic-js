3d902cffd84f21b1061dd6e088abb4440ba2467f
Sartre666 <delphi100500@gmail.com>
2025-01-03 18:35:44 +0300

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error(`'arr' parameter must be an instance of the Array!`);
+  }
+
+  const transformedArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const currentElement = arr[i];
+
+    switch (currentElement) {
+      case '--discard-next':
+        i++; 
+        break;
+      case '--discard-prev':
+        if (i > 0 && arr[i-2] !== '--discard-next') {
+          transformedArray.pop(); 
+        }
+        break;
+      case '--double-next':
+        if (i < arr.length - 1) {
+          transformedArray.push(arr[i + 1]);
+        }
+        break;
+      case '--double-prev':
+        if (i > 0 && arr[i-2] !== '--discard-next') {
+          transformedArray.push(transformedArray[transformedArray.length - 1]);
+        }
+        break;
+      default:
+        transformedArray.push(currentElement);
+    }
+  }
+
+  return transformedArray;
 }
 
 module.exports = {
